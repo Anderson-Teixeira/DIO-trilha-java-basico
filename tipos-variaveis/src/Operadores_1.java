665e4cf62ac6431fbc110731beed9e67fdbe7731
Anderson Teixeira <anderson18062@gmail.com>
2024-09-10 20:56:15 -0300

    Atualizando o exemplo para commit Operadores_1.java

--- tipos-variaveis/src/Operadores_1.java
+++ tipos-variaveis/src/Operadores_1.java
@@ -2,7 +2,7 @@ public class Operadores_1 {
     public static void main (String[] args) {
         String concatenacao = "?";
         System.out.println(concatenacao);
-        System.out.println();
+        System.out.println("1");
 
 
     }
